dee1919ebc56140b49b9256578b9cb4f199b7d86
Sanjay Nair <sanjayhnair@gmail.com>
2018-03-15 11:07:06 -0400

    Fixed zip file reference'

--- multi-environment/main.tf
+++ multi-environment/main.tf
@@ -33,7 +33,7 @@ resource "aws_lambda_function" "demo_lambda" {
   handler          = "index.handler"
   runtime          = "nodejs4.3"
   filename         = "function.zip"
-  source_code_hash = "${base64sha256(file("function.zip"))}"
+  source_code_hash = "${base64sha256(file("node-function/function.zip"))}"
   role             = "${aws_iam_role.lambda_exec_role.arn}"
 }
 
